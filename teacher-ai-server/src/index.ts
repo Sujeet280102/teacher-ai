459dfdcfa0757265b14a443cc36206c1e48b1576
Sujeet280102 <guptasujeet52615@gmail.com>
2025-04-22 05:07:26 +0000

    feat(server): server is created using express and typescript

---
+++ teacher-ai-server/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express()
+
+const PORT = Number(process.env.PORT) || 3000
+
+app.get('/', (req, res) => {
+    res.json(`Server is up and running`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+    
+})
